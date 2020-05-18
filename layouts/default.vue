1d802a2583fa0a80fbcaff4e9606e4d35f84fae7
mak4026 <chikuwa2135@gmail.com>
2020-05-19 08:52:12 +0900

    update for deploy

--- layouts/default.vue
+++ layouts/default.vue
@@ -105,6 +105,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+                {
+          icon: 'mdi-chart-bubble',
+          title: 'Ranking',
+          to: '/ranking'
         }
       ],
       miniVariant: false,
