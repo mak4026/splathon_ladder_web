6c1abacb20172864c611e89ae542958b6e7741c5
mak4026 <chikuwa2135@gmail.com>
2020-05-30 18:12:06 +0900

    編集できるようにした

--- pages/challenges.vue
+++ pages/challenges.vue
@@ -16,7 +16,53 @@
           :hide-default-footer="true"
           sort-by="ChallengerRank"
           class="elevation-1 ranking-table"
-        ></v-data-table>
+        >
+          <template v-slot:top>
+            <v-dialog v-model="dialog" max-width="600px">
+              <v-card>
+                <v-card-title>
+                  <span class="headline">対戦データの編集</span>
+                </v-card-title>
+                <v-card-subtitle>
+                  <span>{{ getStreamTitle }}</span>
+                </v-card-subtitle>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12">
+                        <v-text-field v-model="editedItem.Date" label="開始日時"></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="editedItem.ChallengerScore"
+                          :label="'挑戦者スコア（'+editedItem.Challenger+'）'"
+                          :rules="[rules.score]"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="editedItem.DefenderScore"
+                          :label="'防衛者スコア（'+editedItem.Defender+'）'"
+                          :rules="[rules.score]"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">キャンセル</v-btn>
+                  <v-btn class="primary" color="darken-1" text @click="update">更新</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </template>
+          <template v-slot:item.actions="{ item }">
+            <v-icon small @click="editItem(item)">mdi-pencil</v-icon>
+          </template>
+        </v-data-table>
       </v-card>
     </v-container>
   </v-layout>
@@ -28,26 +74,47 @@ export default {
   data() {
     return {
       loading: true,
+      dialog: false,
       search: "",
       headers: [
         { text: "試合コード", value: "GameId", sort: this.gameIdSort },
+        { text: "開始日時", value: "Date" },
         { text: "ディビジョン", value: "Division" },
         { text: "挑戦者順位", value: "ChallengerRank" },
         { text: "挑戦チーム", value: "Challenger" },
         { text: "挑戦者スコア", value: "ChallengerScore" },
         { text: "防衛者順位", value: "DefenderRank" },
         { text: "防衛チーム", value: "Defender" },
-        { text: "防衛者スコア", value: "DefenderScore" }
+        { text: "防衛者スコア", value: "DefenderScore" },
+        { text: "編集", value: "actions" }
       ],
       season: 5,
-      round: 1
+      round: 1,
+      editedItem: {
+        GameId: "",
+        Date: new Date(),
+        Division: "",
+        ChallengerRank: 0,
+        Challenger: "",
+        ChallengerScore: 0,
+        DefenderRank: 0,
+        Defender: "",
+        DefenderScore: 0
+      },
+      rules: {
+        score: x => (0 <= x && x <= 4) || "0から4の整数を入力してください"
+      }
     };
   },
   computed: {
     ...mapState({
       challenges: state => state.challenges.challenges
     }),
-    ...mapGetters("challenges", ["getChallengesByRound"])
+    ...mapGetters("challenges", ["getChallengesByRound"]),
+    getStreamTitle: function() {
+      const challenge = this.editedItem;
+      return `[${challenge.GameId}] ${challenge.Challenger} vs ${challenge.Defender}`;
+    }
   },
   mounted() {
     this.getChallenges({ season: this.season }).then(
@@ -56,13 +123,35 @@ export default {
   },
   methods: {
     ...mapActions({
-      getChallenges: "challenges/getChallenges"
+      getChallenges: "challenges/getChallenges",
+      updateChallenge: "challenges/updateChallenge",
     }),
     gameIdSort: (a, b) => {
       const reg = /^.*\d+-(\d+).*$/;
       const aCode = reg.exec(a)[1];
       const bCode = reg.exec(b)[1];
       return aCode - bCode;
+    },
+    editItem(item) {
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+    update() {
+      const id = this.editedItem.GameId;
+      const payload = {
+        Date: this.editedItem.Date || "",
+        ChallengerScore: this.editedItem.ChallengerScore,
+        DefenderScore: this.editedItem.DefenderScore
+      };
+      this.updateChallenge({
+        id: id,
+        season: this.season,
+        challenge: payload
+      });
+      this.dialog = false;
+    },
+    close() {
+      this.dialog = false;
     }
   }
 };
--- store/challenges.js
+++ store/challenges.js
@@ -10,24 +10,44 @@ export const actions = {
     const res = await db.collection(`tournaments/spladder${season}/challenges`).get()
     let list = [];
     res.forEach(challengeRef => {
-      const data = {
+      let data = {
         GameId: challengeRef.id,
         ...challengeRef.data()
+      }
+      if(data.Date instanceof firebase.firestore.Timestamp){
+        data.Date = data.Date.toDate();
       }
       list.push(data);
     });
     commit('setChallenges', list);
   },
+  async updateChallenge({ commit }, { id, challenge, season }){
+    const db = firebase.firestore();
+    const challengeRef = db.collection(`tournaments/spladder${season}/challenges`).doc(id);
+    await challengeRef.update({
+      ...challenge,
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+    }).catch(err => console.log(err));
+    commit("updateChallenge", {
+      id: id,
+      challenge: challenge
+    });
+  }
+
 }
 
 export const mutations = {
   setChallenges(state, data) {
     state.challenges = data;
   },
+  updateChallenge(state, data) {
+    let challenge = state.challenges.find(c => c.GameId === data.id);
+    Object.assign(challenge, data.challenge);
+  }
 }
 
 export const getters = {
   getChallengesByRound: (state) => (round) => {
     return state.challenges.filter(c => c.Round === round);
-  }
+  },
 }
