eb6d76b628302c1795886710129af8ece736ab74
mak4026 <chikuwa2135@gmail.com>
2020-05-31 16:46:14 +0900

    add banner to toppage

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,15 @@
   <v-layout column justify-center align-center>
     <v-container fluid>
       <v-card>
-        <v-card-title class="headline">Spladder Portal</v-card-title>
+        <v-img
+          src="./ladder-banner.png"
+          class="white--text align-end"
+          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          max-height="300px"
+          min-height="200px"
+        >
+          <v-card-title class="headline">Spladder Portal</v-card-title>
+        </v-img>
         <v-card-subtitle>Splathon Ladder League Portal Page</v-card-subtitle>
         <v-card-text>
           <p>Season5 は 2020/06/01 からスタート！</p>
