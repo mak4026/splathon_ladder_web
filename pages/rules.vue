07f9baf3168142035b838548fd18f623d1f38b96
mak4026 <chikuwa2135@gmail.com>
2020-06-04 23:19:57 +0900

    スケルトンローダーとトランジションをいれた

--- pages/rules.vue
+++ pages/rules.vue
@@ -2,23 +2,35 @@
   <v-layout column justify-center align-center>
     <v-container>
       <v-card v-if="rules">
-        <v-tabs v-model="tab" grow>
-          <v-tab v-for="rule in rules" :key="rule.round">Round{{ rule.round }}</v-tab>
-        </v-tabs>
-        <v-tabs-items v-model="tab">
-          <v-tab-item v-for="rule in rules" :key="rule.round">
-            <v-card flat>
-              <v-data-table
-                :items="settingWithIndex(rule.settings)"
-                :headers="headers"
-                :loading="loading"
-                disable-sort
-                disable-pagination
-                hide-default-footer
-              ></v-data-table>
-            </v-card>
-          </v-tab-item>
-        </v-tabs-items>
+        <v-skeleton-loader
+          :loading="loading"
+          transition="fade-transition"
+          type="list-item"
+        >
+          <v-tabs v-model="tab" grow>
+            <v-tab v-for="rule in rules" :key="rule.round">Round{{ rule.round }}</v-tab>
+          </v-tabs>
+        </v-skeleton-loader>
+        <v-skeleton-loader
+          :loading="loading"
+          type="image"
+          transition="scroll-x-transition"
+        >
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="rule in rules" :key="rule.round">
+              <v-card flat>
+                <v-data-table
+                  :items="settingWithIndex(rule.settings)"
+                  :headers="headers"
+                  :loading="loading"
+                  disable-sort
+                  disable-pagination
+                  hide-default-footer
+                ></v-data-table>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-skeleton-loader>
       </v-card>
     </v-container>
   </v-layout>
@@ -48,7 +60,6 @@ export default {
       this.loading = false;
       this.tab = this.rules.length - 1;
     })
-
   },
   methods: {
     ...mapActions({
