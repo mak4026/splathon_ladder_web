c778552b2886c789645c5cde8730d6d90e3920ac
mak4026 <chikuwa2135@gmail.com>
2020-06-04 22:32:30 +0900

    ルールをタブわけして表示する

--- pages/rules.vue
+++ pages/rules.vue
@@ -1,8 +1,18 @@
 <template>
   <v-layout column justify-center align-center>
     <v-container>
-      <v-card v-if="rules"></v-card>
-      {{ rules }}
+      <v-card v-if="rules">
+        <v-tabs v-model="tab" grow>
+          <v-tab v-for="rule in rules" :key="rule.round">Round{{ rule.round }}</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item v-for="rule in rules" :key="rule.round">
+            <v-card flat>
+              <v-card-text>{{ rule }}</v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card>
     </v-container>
   </v-layout>
 </template>
@@ -11,22 +21,26 @@ import { mapActions, mapState, mapGetters } from "vuex";
 export default {
   data() {
     return {
+      tab: null,
       loading: true,
-      season: 5,
-    }
+      season: 5
+    };
   },
   computed: {
     ...mapState({
-      rules: state => state.rules.rules,
-    }),
+      rules: state => state.rules.rules
+    })
   },
-  mounted () {
-    this.getRules({season : this.season}).then(() => this.loading = false );
+  async fetch({ store }){
+    await store.dispatch("rules/getRules");
+  },
+  mounted() {
+    this.tab = this.rules.length - 1;
   },
   methods: {
     ...mapActions({
-      getRules: 'rules/getRules',
-    }),
-  },
-}
+      getRules: "rules/getRules"
+    })
+  }
+};
 </script>
